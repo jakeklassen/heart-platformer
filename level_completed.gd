97ed5b54925f06bedebd686d2411a2874831dbe7
Jake <jklassendev@gmail.com>
2024-01-25 09:22:48 -0500

    victory screen

--- level_completed.gd
+++ level_completed.gd
@@ -1,9 +1,11 @@
 extends ColorRect
 
 @onready var retry_button: Button = %RetryButton
+@onready var next_level_button: Button = %NextLevelButton
 
 signal retry()
 signal next_level()
+
 
 func _on_retry_button_pressed() -> void:
 	retry.emit()
--- world.gd
+++ world.gd
@@ -11,11 +11,15 @@ var level_time = 0.0
 var start_level_msec = 0.0
 
 func _ready() -> void:
+	if not next_level is PackedScene:
+		level_completed.next_level_button.text = "Victory Screen"
+		next_level = load("res://victory_screen.tscn")
+
 	Events.level_completed.connect(show_level_completed)
 
 	get_tree().paused = true
 	start_in.visible = true
-	await LevelTransition.fade_from_black()
+	LevelTransition.fade_from_black()
 	animation_player.play("countdown")
 	await animation_player.animation_finished
 	get_tree().paused = false
